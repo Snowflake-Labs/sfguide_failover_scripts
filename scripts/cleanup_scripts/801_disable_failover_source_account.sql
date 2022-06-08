b98ec940399227994d4e12cf96370939d3cf89d9
sfc-gh-pparashar <78575299+sfc-gh-pparashar@users.noreply.github.com>
2022-06-08 15:49:29 -0400

    Create 801_disable_failover_source_account.sql
    Script for disabling failover on the source account

---
+++ scripts/cleanup_scripts/801_disable_failover_source_account.sql
@@ -0,0 +1,5 @@
+use role accountadmin;
+alter connection sfsummitfailover disable failover to accounts <orgname.target_account_name>;
+drop connection sfsummitfailover;
+
+drop failover group sales_payroll_failover;
