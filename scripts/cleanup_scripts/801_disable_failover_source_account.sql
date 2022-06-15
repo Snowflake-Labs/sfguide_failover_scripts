2df72406e15ad8fcc8ba2ce3556d26d76d17faeb
sfc-gh-pparashar <78575299+sfc-gh-pparashar@users.noreply.github.com>
2022-06-14 18:28:32 -0700

    Update 801_disable_failover_source_account.sql
    Added comment

--- scripts/cleanup_scripts/801_disable_failover_source_account.sql
+++ scripts/cleanup_scripts/801_disable_failover_source_account.sql
@@ -1,3 +1,4 @@
+#Drop FG and connection on the source account
 use role accountadmin;
 alter connection sfsummitfailover disable failover to accounts <orgname.target_account_name>;
 drop connection sfsummitfailover;
